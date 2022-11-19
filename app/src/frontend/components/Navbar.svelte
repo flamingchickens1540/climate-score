195ec8eff8bd20d162a17f91de87295daa4fa690
Ewie21 <elocolburn@comcast.net>
2022-11-18 21:33:29 -0800

    did frontend work on the navbar

---
+++ app/src/frontend/components/Navbar.svelte
@@ -0,0 +1,12 @@
+<script type="typescript">
+    import Hamburger from "./Hamburger.svelte";
+
+    export let sidebar = false;
+</script>
+
+<!-- svelte-ignore missing-declaration -->
+<header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
+    <nav class="flex">
+        <Hamburger bind:open={sidebar}/>
+    </nav>
+</header>
--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-import App from './frontend/templates/mainpage.svelte';
+import App from './frontend/templates/Mainpage.svelte';
 
 const app = new App({
 	target: document.body,
