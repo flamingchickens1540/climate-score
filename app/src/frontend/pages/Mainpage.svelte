0cc47dea65122aa015f1ba9de03791564c3a794e
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2022-12-05 21:42:59 -0800

    minor

--- app/src/frontend/pages/Mainpage.svelte
+++ app/src/frontend/pages/Mainpage.svelte
@@ -1,68 +1,69 @@
     <script lang="ts">
-        import { getWalkScore } from '../../backend/apis/walkScore';
-        //import { getAvgCarbonFootprint } from '../../backend/apis/wattBuy';        
-        import ClimateForm from '../components/ClimateForm.svelte';
-        import Box from '../components/Box.svelte';
-        import Navbar from '../components/Navbar.svelte';
-        import ClimateScoreDisplay from '../components/ClimateScoreDisplay.svelte';
-        import { returnRenderForm } from '../components/ClimateForm.svelte';
-        //for some reason it recognizes the file path and the function, but cannot resolve the import
-        //TODO: write frontend nice-ification functions for the data; build out the frontend to look nice
-        //TODO: turn backend into an actual backend
-        //TODO: fix api issues: 503 errors
-        export var walkScore = 0;
-        export var wattData = 0;
+    import { getWalkScore } from '../../backend/apis/walkScore';
+//import { getAvgCarbonFootprint } from '../../backend/apis/wattBuy';        
+import ClimateForm from '../components/ClimateForm.svelte';
+import Box from '../components/Box.svelte';
+import Navbar from '../components/Navbar.svelte';
+import ClimateScoreDisplay from '../components/ClimateScoreDisplay.svelte';
+import { returnRenderForm } from '../components/ClimateForm.svelte';
+//for some reason it recognizes the file path and the function, but cannot resolve the import
+//TODO: write frontend nice-ification functions for the data; build out the frontend to look nice
+//TODO: turn backend into an actual backend
+//TODO: fix api issues: 503 errors
+export var walkScore = 0;
+export var wattData = 0;
 
-        //Test Function
-        //Complete 
-        //TODO: Remove
-        function test(){
-            console.log("test");
-        }
-        //Remove the WattBuy Data from the Screen 
-        //Status: Complete
-        function removeWattBuyData(){
-            wattData = 0;
-        }
+//Test Function
+//Complete 
+//TODO: Remove
+function test(){
+    console.log("test");
+}
+//Remove the WattBuy Data from the Screen 
+//Status: Complete
+function removeWattBuyData(){
+    wattData = 0;
+}
 
-        //gets the walkscore data and displays it on the screen
-        //Status: Incomplete
+//gets the walkscore data and displays it on the screen
+//Status: Incomplete
 
-        function testWalkScoreData(){
-            getWalkScore("address=1515%Umatilla%20St%20Portland%20OR%97202&lat=45.463100&lon=-122.650520&transit=1&bike=1&wsapikey=")
-            .then((walkScore) => {
-                console.log(walkScore);
-                  walkScore = walkScore;
-            });
+function testWalkScoreData(){
+    getWalkScore("address=1515%Umatilla%20St%20Portland%20OR%97202&lat=45.463100&lon=-122.650520&transit=1&bike=1&wsapikey=")
+    .then((walkScore) => {
+        console.log(walkScore);
+          walkScore = walkScore;
+    });
+}
+////Gets the watt buy data and displays it on the screen
+//Status: Incomplete
+function testWattBuyData(){
+    /*
+    getAvgCarbonFootprint("address=1515&city=Portland&state=Or&zip=97202")
+    .then((carbon) => {
+        console.log(carbon);
+        wattData = carbon;
+    });   
+    */
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            'x-api-key': 'rsFQKFKcYk9FOyZuaNne12QHdHeRACtOCT29m5uh'
         }
-        ////Gets the watt buy data and displays it on the screen
-        //Status: Incomplete
-        function testWattBuyData(){
-            /*
-            getAvgCarbonFootprint("address=1515&city=Portland&state=Or&zip=97202")
-            .then((carbon) => {
-                console.log(carbon);
-                wattData = carbon;
-            });   
-            */
-            const options = {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    'x-api-key': 'rsFQKFKcYk9FOyZuaNne12QHdHeRACtOCT29m5uh'
-                }
-            };
+    };
 
-            fetch('https://apis.wattbuy.com/v3/electricity/carbon-footprint?address=1515&city=Portland&state=Or&zip=97202', options)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response);
-                wattData = response;
-            })
-            .catch(err => console.error(err));
-        }
-        let renderForm = returnRenderForm();
-    </script>
+    fetch('https://apis.wattbuy.com/v3/electricity/carbon-footprint?address=1515&city=Portland&state=Or&zip=97202', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        wattData = response;
+    })
+    .catch(err => console.error(err));
+}
+let renderForm = returnRenderForm();
+
+</script>
     <!-- svelte-ignore missing-declaration -->
     <Navbar></Navbar>
     <!-- svelte-ignore missing-declaration -->
--- app/src/frontend/svelteScripts/mainpageTs.ts
+++ app/src/frontend/svelteScripts/mainpageTs.ts
@@ -1,14 +1,16 @@
 import { getWalkScore } from '../../backend/apis/walkScore';
-import { getAvgCarbonFootprint } from '../../backend/apis/wattBuy';        
-// import ClimateForm from '../components/ClimateForm.svelte';
-// import Box from '../components/Box.svelte';
-//TODO: figure out why importation isn't working even though it recognizes the file pathname
+//import { getAvgCarbonFootprint } from '../../backend/apis/wattBuy';        
+import ClimateForm from '../components/ClimateForm.svelte';
+import Box from '../components/Box.svelte';
+import Navbar from '../components/Navbar.svelte';
+import ClimateScoreDisplay from '../components/ClimateScoreDisplay.svelte';
+import { returnRenderForm } from '../components/ClimateForm.svelte';
 //for some reason it recognizes the file path and the function, but cannot resolve the import
 //TODO: write frontend nice-ification functions for the data; build out the frontend to look nice
-
-
-var score = 0;
-var wattData = 0;
+//TODO: turn backend into an actual backend
+//TODO: fix api issues: 503 errors
+export var walkScore = 0;
+export var wattData = 0;
 
 //Test Function
 //Complete 
@@ -16,35 +18,46 @@ var wattData = 0;
 function test(){
     console.log("test");
 }
-//open a settings manu
-//Status: Incomplete
-//TODO: write function
-function handleMenu(){
-    console.log("handleMenu");
-}
 //Remove the WattBuy Data from the Screen 
 //Status: Complete
 function removeWattBuyData(){
     wattData = 0;
 }
-//Gets the watt buy data and displays it on the screen
-//Status: Complete
-
-function testWattBuyData(){
-    getAvgCarbonFootprint("address=1515&city=Portland&state=Or&zip=97202")
-    .then((data) => {
-        console.log(data);
-        wattData = data;
-    });
-}
 
 //gets the walkscore data and displays it on the screen
-//Status: Complete
+//Status: Incomplete
 
 function testWalkScoreData(){
-    getWalkScore("https://api.walkscore.com/score?format=json&address=1515%Umatilla%20St%20Portland%20OR%97202&lat=45.463100&lon=-122.650520&transit=1&bike=1&wsapikey=")
-    .then((data) => {
-        console.log(data);
-        score = data;
+    getWalkScore("address=1515%Umatilla%20St%20Portland%20OR%97202&lat=45.463100&lon=-122.650520&transit=1&bike=1&wsapikey=")
+    .then((walkScore) => {
+        console.log(walkScore);
+          walkScore = walkScore;
     });
 }
+////Gets the watt buy data and displays it on the screen
+//Status: Incomplete
+function testWattBuyData(){
+    /*
+    getAvgCarbonFootprint("address=1515&city=Portland&state=Or&zip=97202")
+    .then((carbon) => {
+        console.log(carbon);
+        wattData = carbon;
+    });   
+    */
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            'x-api-key': 'rsFQKFKcYk9FOyZuaNne12QHdHeRACtOCT29m5uh'
+        }
+    };
+
+    fetch('https://apis.wattbuy.com/v3/electricity/carbon-footprint?address=1515&city=Portland&state=Or&zip=97202', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        wattData = response;
+    })
+    .catch(err => console.error(err));
+}
+let renderForm = returnRenderForm();
