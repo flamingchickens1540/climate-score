cda2b79b0e7a0fd4bc9e70a5e3fb7a47dd9a7f96
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-01-07 11:15:31 -0800

    Removed app folder

--- app/client/src/lib/ScoreError.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let error;
-</script>
-
-<h1>THERE WAS AN ERROR ON NO!</h1>
-<p>{error}</p>
--- app/client/src/lib/ScoreLoading.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import DoubleBounce from "./DoubleBounce.svelte";
-
-</script>
-<h1>YOUR SCORE IT LOADING, WHAT WILL IT BE?</h1>
-<DoubleBounce></DoubleBounce>
