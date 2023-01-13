e8c560cdd6003426b5cf805f3ff30528f981c424
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-01-13 15:32:20 -0800

    Jack, this is for you, don't commit anything without testing

---
+++ client/common/types.ts
@@ -0,0 +1,37 @@
+export type AddressData = {
+    address: string;
+    cityName: string;
+    state: string;
+    street: string;
+    kindOfStreet: string;
+    cardinal: string;
+    zip: string;
+};
+
+export type Coords = {
+    lat: number,
+    long: number
+}
+
+export type Res = {
+    status?: number,
+    walkScore?: number,
+    walkScoreDescription?: string,
+    estimatedGenerationData?: {},
+    carbonFootprint?: number,
+    error?: boolean,
+    message?: string
+}
+
+export type WattBuyResponse = {
+    estimatedGenerationData?: {},
+    carbonFootprint?: number
+}
+
+export type SpinnerTypes = {
+	size: string | number;
+	color: string;
+	unit: string;
+	duration: string;
+	pause: boolean;
+};
